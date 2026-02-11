4ebb6720f2e15cd7140af1bf14e8e78b578290e6
kiff 540 <kibyakiff@gmail.com>
2026-02-12 00:26:49 +0300

    practice.py

--- practice.py
+++ practice.py
@@ -1,7 +1,29 @@
-line1 = "********************"
-line2 = "*     WELCOME      *"
-line3 = "********************"
+# CREATING AN IF STATEMENT
+# meaning = 8
+# print("")
 
-print(line1)
-print(line2)
-print(line3)
+# if meaning > 10:
+#     print("Yes")
+# else:
+#     print("No")
+
+# TERNARY OPERATOR
+# print("Yes") if meaning > 10 else print("No")
+
+#CONCATANATION
+# firstname = "Mwenje"
+# secondname = "Kiff"
+# fullname = firstname + " " + secondname
+# print (fullname)
+
+# STRING MSETHOD
+# print (firstname)
+# print (firstname.upper())
+# print (firstname.lower())
+
+# STRING INDEX METHODS
+print(firstname[0])
+print(firstname[5])
+print(firstname[-1])
+print(firstname[0:5])
+print(firstname[0:])
