d4d14027c88529399c0daf3c63e01808e8610633
kiff540 <kibyakiff@gmail.com>
2026-02-11 09:17:15 +0300

    first commit

---
+++ challenge1.py
@@ -0,0 +1,7 @@
+university = "STANFORD"
+grade = "A"
+clusterpoints = "43"
+course = "Aviation"
+print(f"My favourite uni is {university}")
+print(f"I acquired an {grade} grade")
+print(f"I would like to pursue {course} whose cluster points are {clusterpoints}") 
---
+++ hello.py
@@ -0,0 +1,14 @@
+# This is a comment
+print("Hello from kiff")
+#city
+print ("I am from kiambu")
+# my hobbies
+print("I like staying home,making money,learning")
+
+age = 18 
+height = "2.5 m"
+weight= 67
+
+print (f"I am {age} old")
+print (f"I am also {height} tall")
+print (f"And weigh {weight} kgs")
