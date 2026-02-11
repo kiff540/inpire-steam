e663045bfec6fc4cb24f819273985ec1186190c0
kiff 540 <kibyakiff@gmail.com>
2026-02-11 22:28:50 +0300

    no input.py

---
+++ no input.py
@@ -0,0 +1,16 @@
+# Mwenje kiff
+# 11/02/2026
+# Program to add,subtract,divide and multiply
+
+first_number = int(input("Enter the first number"))
+second_number = int(input("Enter the second number"))
+
+sum_number = first_number + second_number
+difference_number = first_number - second_number
+multiplication_number = first_number * second_number
+division_number = first_number / second_number
+
+print (f"the sum of the numbers is {sum_number}")
+print (f"the difference of the numbers is {difference_number}")
+print (f"the multiplication of the numbers is {multiplication_number}")
+print (f"the division of the numbers is {division_number}")
